6f7352cf2c3f88ca165b44d8f8f2d49ea9300c1a
Magnus Morton <magnusmorton@gmail.com>
2012-03-06 20:36:32 +0000

    changed tests somewhat

--- tests/test_py_pip.py
+++ tests/test_py_pip.py
@@ -3,12 +3,27 @@ from py_pip import *
 
 class TestProblem(unittest.TestCase):
     
+    INDEP = [[1, 0, 0], [-1, 0, 10],[0, 1, 0],[0, -1, 10],[1, -1, 0],[-1, 1, 0],[1, -1, -1],[-1, 1, -1]]
+            
+            
+
     def setUp(self):
         self.problem = Problem()
         
     def test_can_create_problem(self):
-        
         assert self.problem is not None, "Problem doesn't exist"
         
-    # def test_add_constraints(self):
-    #         a
+    def test_number_of_unknowns_set(self):
+        problem = Problem(2)
+        assert problem.unknowns_count == 2, "Wrong number of unknowns"
+        
+    def test_number_of_unknowns_set_when_3(self):
+           problem = Problem(3)
+           assert problem.unknowns_count == 3, "Wrong number of unknowns"
+           
+    
+    def test_basic_independent(self):
+        """Apologies for the multiple asserts"""
+        problem = Problem(2)
+        problem.domain = TestProblem.INDEP
+        assert problem.domain == TestProblem.INDEP, "Cannot assign domain"
