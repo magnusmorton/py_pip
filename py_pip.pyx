3abf3b658149bc7b94e48c479e0786305f240cba
Magnus Morton <magnusmorton@gmail.com>
2012-03-06 20:36:47 +0000

    added domain attribute

--- py_pip.pyx
+++ py_pip.pyx
@@ -1,6 +1,26 @@
 cimport pip
 
 cdef class Problem:
+    cdef int _unknowns_count
+    cdef _domain
+    def __cinit__(self, unknowns_count=0):
+        self._domain = []
+        self._unknowns_count = unknowns_count
+        
+    @property
+    def unknowns_count(self):
+        return self._unknowns_count
+        
     
-    def __cinit__(self):
-        pass
+    def get_domain(self):
+        return self._domain
+        
+    def set_domain(self, matrix):
+        self._domain = matrix
+        
+    domain = property(get_domain, set_domain)
+        
+    
+        
+        
+    
