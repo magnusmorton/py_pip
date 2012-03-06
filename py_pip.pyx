d6595f492fe2ed5a47678e8853200f93499c66a5
Magnus Morton <magnusmorton@gmail.com>
2012-03-06 22:14:05 +0000

    beginnings of solve()

--- py_pip.pyx
+++ py_pip.pyx
@@ -1,12 +1,15 @@
 cimport pip
+cimport libc.stdio
 
 cdef class Problem:
     cdef int _unknowns_count
-    cdef _domain
-    cdef pip.PipMatrix _problem
+    cdef _py_domain
+    cdef pip.PipMatrix *_domain
+    cdef pip.PipMatrix *_context
+    cdef pip.PipOptions *_options
     
     def __cinit__(self, unknowns_count=0):
-        self._domain = []
+        self._py_domain = []
         self._unknowns_count = unknowns_count
         
     @property
@@ -14,12 +17,26 @@ cdef class Problem:
         return self._unknowns_count
     
     def get_domain(self):
-        return self._domain
+        return self._py_domain
         
     def set_domain(self, matrix):
-        self._domain = matrix
+        self._py_domain = matrix
         
     domain = property(get_domain, set_domain)
+    
+    
+    def solve(Problem self):
+        cdef int rows, cols
+        rows = len(self._py_domain)
+        cols = len(self._py_domain[0])
+        self._domain = pip.pip_matrix_alloc(rows, cols)
+        for i in range(rows):
+            for j in range(cols):
+                self._domain.p[i][j] = self._py_domain[i][j]
+                
+    
+    def dump(self):
+        pip.pip_matrix_print(libc.stdio.stdout, self._domain)
         
     
         
