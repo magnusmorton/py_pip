741b23b00a0e949f209777160ba029ad537c02e1
Magnus Morton <magnusmorton@gmail.com>
2012-03-07 17:38:32 +0000

    added tests for dependency solutions

--- tests/test_py_pip.py
+++ tests/test_py_pip.py
@@ -4,6 +4,8 @@ from py_pip import *
 class TestProblem(unittest.TestCase):
     
     INDEP = [[1, 1, 0, 0], [1, -1, 0, 10],[1,0, 1, 0],[1, 0, -1, 10],[0, 1, -1, 0],[1, 1, -1, -1],[1, -1, 1, -1]]
+    DEP = [[1, 1, 0, 0], [1, -1, 0, 10],[1,0, 1, 0],[1, 0, -1, 10],[0, 1, -1, -1]]
+    
             
             
 
@@ -27,10 +29,20 @@ class TestProblem(unittest.TestCase):
         problem.domain = TestProblem.INDEP
         problem.solve()
         problem.dump()
-           
-    
+        
+    def test_no_solution_when_independent(self):
+        problem = Problem(2)
+        problem.domain = TestProblem.INDEP
+        solution = problem.solve()
+        assert not solution.solution_exists, "solution found when none exists"
+        
+    def test_solution_when_dependent(self):
+        problem = Problem(2)
+        problem.domain = TestProblem.DEP
+        solution = problem.solve()
+        assert solution.solution_exists, "solution not found when one exists"
+        
     def test_basic_independent(self):
-        """Apologies for the multiple asserts"""
         problem = Problem(2)
         problem.domain = TestProblem.INDEP
         assert problem.domain == TestProblem.INDEP, "Cannot assign domain"
