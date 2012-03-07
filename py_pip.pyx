9c98f4dbd836d8c2ed6245ecea64e88f907e278e
Magnus Morton <magnusmorton@gmail.com>
2012-03-07 17:38:42 +0000

    added solution class

--- py_pip.pyx
+++ py_pip.pyx
@@ -37,12 +37,28 @@ cdef class Problem:
                 self._domain.p[i][j] = self._py_domain[i][j]
                 
         sol = pip.pip_solve(self._domain, NULL,-1, self._options)
+        wrapped_solution = Solution()
+        wrapped_solution.add_solution(sol)
         pip.pip_quast_print(libc.stdio.stdout, sol, 1)
-                
+        libc.stdio.printf("%d\n",sol.list)
+        return wrapped_solution
     
     def dump(self):
         pip.pip_matrix_print(libc.stdio.stdout, self._domain)
         
+
+cdef class Solution:
+    cdef pip.PipQuast *_solution
+    
+    cdef add_solution(self, pip.PipQuast *solution):
+        self._solution = solution
+    
+    @property
+    def solution_exists(self):
+        if self._solution.condition is not NULL or self._solution.list is not NULL:
+            return True
+        else:
+            return False
     
         
         
